86adf92cf815e6c9a994a719365abc7c30f86951
Tynybek <tynybeko1122@gmail.com>
2023-09-14 00:38:48 +0600

    burgermenu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -40,8 +40,10 @@ export default function Header() {
         <header className="header">
             <div className="container">
                 <div className="header--body">
+
+
                     <div className="header--body--logo">
-                        <Link onClick={()=> {
+                        <Link onClick={() => {
                             setBurgerState(false)
                         }} href={'/'}>
                             <img src="/assets/img/site--logo.png" alt="logo" />
@@ -70,21 +72,26 @@ export default function Header() {
                         </div>
                         <div className="header--body--menu">
                             <BurgerMenu setBurger={setBurgerState} navs={categories} isBurger={isBurger} />
-                            {
-                                !isBurger ? <img onClick={() => {
-                                    setBurgerState(true)
-                                }} className="open" src="/assets/svg/burger--menu--open.svg" alt="burger--button" /> :
-                                    <img onClick={() => {
+                            <div className="burger-menu">
+                                <div className="inner">
+                                    <button className={`menu ${isBurger ? 'opened' : ''}`} onClick={() => {
+                                        setBurgerState(prev => !prev)
+                                    }} aria-expanded={isBurger} aria-label="Main Menu">
+                                        <svg width="40" height="40" viewBox="0 0 100 100">
+                                            <path className="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+                                            <path className="line line2" d="M 20,50 H 80" />
+                                            <path className="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+                                        </svg>
+                                    </button>
+                                </div>
 
-                                        setBurgerState(false)
-                                    }} className="open" src="/assets/svg/close--burger--menu.svg" alt="burger--button" />
-                            }
+                            </div>
 
                         </div>
                     </div>
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
 
